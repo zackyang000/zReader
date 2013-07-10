d8c09f6b7be082fccd65dabfcaa8dd9904d705e3
TossShinHwa <yangkai-13896222@sohu.com>
2013-07-10 21:28:43 +0800

    Refactor solution.

---
+++ src/Reader.Spider/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reader.Spider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
